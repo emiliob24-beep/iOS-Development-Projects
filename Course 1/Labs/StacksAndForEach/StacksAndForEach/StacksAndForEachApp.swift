4698c68ede1cf7dc4a272bb40f69b58a154e05f4
Emilio Briceno <emiliobj2002@icloud.com>
2026-02-27 10:30:56 -0700

    Re-add stacks lab without submodule

---
+++ Course 1/Labs/StacksAndForEach/StacksAndForEach/StacksAndForEachApp.swift
@@ -0,0 +1,20 @@
+//
+//  StacksAndForEachApp.swift
+//  StacksAndForEach
+//
+//  Created by Emilio Briceno on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct StacksAndForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
